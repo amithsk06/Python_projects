e0961cb88b2be1cf2402ff31eddffc535526c297
Amith S Kumar <amithsk123456@gmail.com>
2023-08-16 09:50:38 +0530

    The Word Guesser

---
+++ BandNameGenerator.py
@@ -0,0 +1,4 @@
+print("Welcome to Band Name Generator.")
+name=input("What's city you grew up in? \n")
+pet_name=input("What's your pet name? \n")
+print("Your Band name can be " + name +" " + pet_name +"\n")
